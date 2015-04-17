ee5953ccaa652a78e1bb4935f5a13e7717cd2e4a
Alireza Savand <alireza.savand@gmail.com>
2015-04-17 19:05:09 +0400

    Add tiwun.account.module

---
+++ app/scripts/account/module.js
@@ -0,0 +1,13 @@
+(function () {
+  'use strict';
+
+  angular.module('tiwun.account', [
+    'tiwun.account.controllers',
+    'tiwun.account.services'
+  ]);
+
+  angular.module('tiwun.account.controllers', []);
+  angular.module('tiwun.account.services', []);
+  angular.module('tiwun.account.services', ['ngCookies']);
+
+})();
