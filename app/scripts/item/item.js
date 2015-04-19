3c6bd54ff7fa693f166c1d8839d9bf24d7cf4e11
Alireza Savand <alireza.savand@gmail.com>
2015-04-19 17:19:11 +0400

    Add items module

---
+++ app/scripts/item/item.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    angular.module('tiwun.item', [
+        'tiwun.item.controllers',
+        'tiwun.item.directives',
+        'tiwun.item.services'
+    ]);
+
+    angular.module('tiwun.item.controllers', []);
+    angular.module('tiwun.item.directives,' ['ngDialog'])
+})();
+
