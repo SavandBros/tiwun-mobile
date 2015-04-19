19e8108bd559ff779a42cfd4aba47aeca970d705
Alireza Savand <alireza.savand@gmail.com>
2015-04-19 14:07:11 +0400

    Add ItemService

---
+++ app/scripts/item/services/item_service.js
@@ -0,0 +1,64 @@
+/**
+ * ItemService
+ * @namespace tiwun.item.services
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('tiwun.item.services')
+        .factory('ItemService', ItemService);
+
+    ItemService.$inject = ['$http'];
+
+    /**
+     * @namespace ItemService
+     * @returns {Factory}
+     */
+    function ItemService($http) {
+        var ItemService = {
+            all: all,
+            get: get,
+            create: create
+        };
+
+        return ItemService;
+
+
+        /**
+         * @name all
+         * @desc Get all ItemService
+         * @returns {Promise}
+         * @memberOf tiwun.item.services.ItemService
+         */
+        function all() {
+            return $http.get('/api/v1/items/');
+        }
+
+
+        /**
+         * @name create
+         * @desc Create a new Post
+         * @param {string} content The content of the new Post
+         * @returns {Promise}
+         * @memberOf tiwun.item.services.ItemService
+         */
+        function create(content) {
+            return $http.post('/api/v1/items/', {
+                content: content
+            });
+        }
+
+
+        /**
+         * @name get
+         * @desc Get the ItemService of a given user
+         * @param {string} username The username to get ItemService for
+         * @returns {Promise}
+         * @memberOf tiwun.item.services.ItemService
+         */
+        function get(id) {
+            return $http.get('/api/v1/users/' + id + '/items/');
+        }
+    }
+})();
