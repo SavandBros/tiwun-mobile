d388280db52d5585bc658ed4bfa61c59ba640268
Alireza Savand <alireza.savand@gmail.com>
2015-04-19 17:18:24 +0400

    Add Items directive

---
+++ app/scripts/item/directives/items_directive.js
@@ -0,0 +1,31 @@
+/**
+ * Items
+ * @namespace tiwun.items.directive
+ */
+(function () {
+    "use strict";
+
+    angular.module('tiwun.item.directives')
+        .directive('items', items);
+
+    /**
+     * @namespace Items
+     */
+    function items() {
+        /**
+         * @name directive
+         * @desc The directive to be returned
+         * @memberOf tiwun.item.directives.Items
+         */
+        var directive = {
+            controller: 'ItemController',
+            restrict: 'E',
+            scope: {
+                items: '='
+            },
+            templateUrl: 'templates/items/items.html'
+        };
+
+        return directive;
+    }
+})();
