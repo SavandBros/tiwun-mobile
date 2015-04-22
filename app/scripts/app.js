70e1f14c781f32cc0447d6e7d19247b0c4d96f10
Alireza Savand <alireza.savand@gmail.com>
2015-04-23 01:47:45 +0400

    Default is explore now

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -101,5 +101,5 @@ angular.module('tiwun', [
                 }
             });
         // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/app/playlists');
+        $urlRouterProvider.otherwise('/app/explore');
     });
