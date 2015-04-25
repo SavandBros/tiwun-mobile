657a6dc59b2292742f7b425f8e359b6be1fe8b57
Alireza Savand <alireza.savand@gmail.com>
2015-04-25 15:47:16 +0400

    Add objectType to api call

--- app/scripts/sushial/services/vote_service.js
+++ app/scripts/sushial/services/vote_service.js
@@ -33,28 +33,40 @@
         /**
          * @name upVote
          * @desc Vote up for the given item.
+         * @param {number} objectType: Object type that is being voted.
          * @param {string} objectId: Object PK!
          * @param {string} userId: User Id that up voting the object.
          * @memberOf tiwun.sushial.servicesVoteService
          */
-        function upVote(objectId, userId) {
+        function upVote(objectType, objectId, userId) {
             return $http.post(
                 'https://127.0.0.1:8000/api/sushial/vote/',
-                {object_id: objectId, user_id: userId, vote_type: VoteTypes.up}
+                {
+                    object_type: objectType,
+                    object_id: objectId,
+                    user_id: userId,
+                    vote_type: VoteTypes.up
+                }
             );
         }
 
         /**
          * @name downVote
          * @desc Down vote for the given item.
+         * @param {number} objectType: Object type that is being voted.
          * @param {string} objectId: Object PK!
          * @param {string} userId: User Id that down voting the object.
          * @memberOf tiwun.sushial.services.VoteService
          */
-        function downVote(objectId, userId) {
+        function downVote(objectType, objectId, userId) {
             return $http.post(
                 'https://127.0.0.1:8000/api/sushial/vote/',
-                {object_id: objectId, user_id: userId, vote_type: VoteTypes.down}
+                {
+                    object_type: objectType,
+                    object_id: objectId,
+                    user_id: userId,
+                    vote_type: VoteTypes.down
+                }
             );
         }
     }
