98dbc221c4f03c95612af2fc0d2442efa00cee45
Alireza Savand <alireza.savand@gmail.com>
2015-04-25 15:43:43 +0400

    Add upVote
    Vote up for the given item.

--- app/scripts/sushial/services/vote_service.js
+++ app/scripts/sushial/services/vote_service.js
@@ -29,5 +29,19 @@
             'comment': 1,
             'tag': 2
         };
+
+        /**
+         * @name upVote
+         * @desc Vote up for the given item.
+         * @param {string} objectId: Object PK!
+         * @param {string} userId: User Id that up voting the object.
+         * @memberOf tiwun.sushial.servicesVoteService
+         */
+        function upVote(objectId, userId) {
+            return $http.post(
+                'https://127.0.0.1:8000/api/sushial/vote/',
+                {object_id: objectId, user_id: userId, vote_type: VoteTypes.up}
+            );
+        }
     }
 })();
