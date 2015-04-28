232dfe5f87036060adeed6591de2349b6f044aa7
Alireza Savand <alireza.savand@gmail.com>
2015-04-28 18:27:34 +0400

    Initial tiwun.search module

---
+++ app/scripts/search/search.js
@@ -0,0 +1,12 @@
+(function () {
+    "use strict";
+
+    angular.module('tiwun.search', [
+        'tiwun.search.controllers',
+        'tiwun.search.services'
+    ]);
+
+
+    angular.module('tiwun.search.controllers', []);
+    angular.module('tiwun.search.services', []);
+})();
