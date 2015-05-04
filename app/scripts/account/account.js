a709ca93653043cb72a3192c1edd729d75ffbb00
Alireza Savand <alireza.savand@gmail.com>
2015-05-04 22:06:16 +0400

    Initialized UserProfileControllerser

--- app/scripts/account/account.js
+++ app/scripts/account/account.js
@@ -6,6 +6,7 @@
         'tiwun.account.controllers.LoginController',
         'tiwun.account.controllers.AccountSettingsController',
         'tiwun.account.controllers.AccountController',
+        'tiwun.account.controllers.UserProfileController',
         'tiwun.account.services.AuthenticationService',
         'tiwun.account.services.UserService'
     ]);
@@ -14,6 +15,7 @@
     angular.module('tiwun.account.controllers.LoginController', []);
     angular.module('tiwun.account.controllers.AccountSettingsController', []);
     angular.module('tiwun.account.controllers.AccountController', []);
+    angular.module('tiwun.account.controllers.UserProfileController', []);
     angular.module('tiwun.account.services.AuthenticationService', ['ngCookies']);
     angular.module('tiwun.account.services.UserService', []);
 
---
+++ app/scripts/account/controllers/user_profile_controller.js
@@ -0,0 +1,27 @@
+/**
+ * UserProfileController
+ * @class UserProfileController
+ * @namespace tiwun.account.controllers
+ */
+(function () {
+    'use strict';
+
+    angular.module('tiwun.account.controllers.UserProfileController', [])
+        .controller('UserProfileController', UserProfileController);
+
+    UserProfileController.$inject = [];
+
+    function UserProfileController() {
+        constructor();
+
+        /**
+         * @name constructor
+         * @desc Actions to be performed when this controller is instantiated
+         * @memberOf UserProfileController
+         */
+        function constructor() {
+
+        }
+    }
+
+})();
