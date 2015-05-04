4328e885ef5e610ed002e313a6fb160bf62ab71d
Alireza Savand <alireza.savand@gmail.com>
2015-05-04 21:59:43 +0400

    Comments and documentation TagService

--- app/scripts/tagool/services/tag_service.js
+++ app/scripts/tagool/services/tag_service.js
@@ -4,34 +4,62 @@
  * @namespace tiwun.tagool.services
  */
 (function () {
-        'use strict';
+    'use strict';
 
-        angular.module('tiwun.tagool.services.TagService', [])
-                .factory('TagService', TagService);
+    angular.module('tiwun.tagool.services.TagService', [])
+        .factory('TagService', TagService);
 
-        TagService.$inject = ['$http'];
+    TagService.$inject = ['$http'];
 
-        function TagService($http) {
-                var TagService = {
-                    all: all,
-                    tagDetail: tagDetail
-                };
 
-                return TagService;
+    /**
+     * TagService
+     * @name TagService
+     * @class TagService
+     * @param $http
+     * @memberOf: tiwun.tagool.services
+     * @returns {{all: TagService.all, tagDetail: TagService.tagDetail}}
+     */
+    function TagService($http) {
+        var TagService = {
+            all: all,
+            tagDetail: tagDetail
+        };
 
-            function all(page_number) {
-                return $http.get(
-                        'https://127.0.0.1:8000/api/tags/',
-                        {params: {page: page_number}}
-                );
-            }
+        return TagService;
 
-            function tagDetail(page_number, tagSlug) {
-                return $http.get(
-                        'https://127.0.0.1:8000/api/tags/'+tagSlug+'/',
-                        {params: {page: page_number}}
-                );
-            }
+
+        /**
+         * all
+         * @method all
+         * @name all
+         * @desc List of all tags.
+         * @param {number} page_number
+         * @memberOf TagService
+         * @returns {Promise}
+         */
+        function all(page_number) {
+            return $http.get(
+                'https://127.0.0.1:8000/api/tags/',
+                {params: {page: page_number}}
+            );
         }
 
+        /**
+         * tagDetail
+         * @method tagDetail
+         * @name tagDetail
+         * @desc Get given tag associated objects.
+         * @param {number} page_number
+         * @param {string} tagSlug
+         * @memberOf TagService
+         * @returns {Promise}
+         */
+        function tagDetail(page_number, tagSlug) {
+            return $http.get(
+                'https://127.0.0.1:8000/api/tags/tagSlug/'.replace('tagSlug', tagSlug),
+                {params: {page: page_number}}
+            );
+        }
+    }
 })();
