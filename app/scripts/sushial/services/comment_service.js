af2bd4c30b9e494eb6053b3f6dadd5a309a3559e
Alireza Savand <alireza.savand@gmail.com>
2015-05-11 15:40:30 +0400

    CommentService is working with no problemo

--- app/scripts/sushial/services/comment_service.js
+++ app/scripts/sushial/services/comment_service.js
@@ -1,83 +1,83 @@
+/*global angular*/
+'use strict';
+
 /**
- * CommentService
- * @namespace tiwun.sushial.services
+ * @class CommentService
+ * @returns {Factory}
+ * @namespace tiwun.sushial.services.CommentService
  */
-(function () {
-    'use strict';
-
-    angular.module('tiwun.sushial.services.CommentService', [])
-        .factory('CommentService', CommentService);
-
-    CommentService.$inject = ['$http'];
+function CommentService($http, ENV) {
+    var ObjectTypes = {
+        'item': 1,
+        'comment': 2,
+        'tag': 3
+    };
 
     /**
-     * @namespace CommentService
-     * @returns {Factory}
+     * @name create: Creating a comment for an object.
+     * @param {number} objectType: Object type that is being commented on.
+     * @param {string} objectPk:  Object PK!
+     * @param {string} userId: User Id that commenting on the the object.
+     * @param {string} commentText: Comment text that is being posted on the object.
+     * @memberOf tiwun.sushial.services.CommentService
      */
-    function CommentService($http) {
-        var CommentService = {
-            create: create,
-            remove: remove,
-            filterByObject: filterByObject
-        };
-
-        var ObjectTypes = {
-            'item': 1,
-            'comment': 2,
-            'tag': 3
-        };
-
-        return CommentService;
-
-        /**
-         * @name create: Creating a comment for an object.
-         * @param {number} objectType: Object type that is being commented on.
-         * @param {string} objectPk:  Object PK!
-         * @param {string} userId: User Id that commenting on the the object.
-         * @param {string} commentText: Comment text that is being posted on the object.
-         * @memberOf tiwun.sushial.services.CommentService
-         */
-        function create(objectType, objectPk, userId, commentText) {
-            return $http.post(
-                'https://127.0.0.1:8000/api/sushial/comment/add/',
-                {
-                    object_type: objectType,
-                    object_pk: objectPk,
-                    user_id: userId,
-                    comment: commentText
-                }
-            );
-        }
-
-        /**
-         * @name remove: Remove a comment from an object.
-         * @param {number} objectType: Object type that is being commented on.
-         * @param {string} objectId:  Object PK!
-         * @param {string} userId: User Id that commenting on the the object.
-         * @memberOf tiwun.sushial.services.CommentService
-         */
-        function remove(objectType, objectId, userId) {
-            return $http.post(
-                'https://127.0.0.1:8000/api/sushial/comment/remove',
-                {
-                    object_type: objectType,
-                    object_id: objectId,
-                    user_id: userId
-                }
-            );
-        }
-
-        /**
-         * @name filterByObject: Filter comments by given object.
-         * @param {number} objectType: Object type that is being commented on.
-         * @param {string} ObjectPk:  Object PK!
-         * @memberOf tiwun.sushial.services.CommentService
-         */
-        function filterByObject(objectType, ObjectPk) {
-            return $http.get(
-                'https://127.0.0.1:8000/api/sushial/comment/',
-                {params:{object_type: objectType, object_pk: ObjectPk}}
-            );
-        }
+    function create(objectType, objectPk, userId, commentText) {
+        return $http.post(
+            '$<% ENV.apiEndpoint %>sushial/comment/add/',
+            {
+                object_type: objectType,
+                object_pk: objectPk,
+                user_id: userId,
+                comment: commentText
+            }
+        );
     }
-})();
+
+    /**
+     * @name remove: Remove a comment from an object.
+     * @param {number} objectType: Object type that is being commented on.
+     * @param {string} objectId:  Object PK!
+     * @param {string} userId: User Id that commenting on the the object.
+     * @memberOf tiwun.sushial.services.CommentService
+     */
+    function remove(objectType, objectId, userId) {
+        return $http.post(
+            'https://127.0.0.1:8000/api/sushial/comment/remove',
+            {
+                object_type: objectType,
+                object_id: objectId,
+                user_id: userId
+            }
+        );
+    }
+
+    /**
+     * @name filterByObject: Filter comments by given object.
+     * @param {number} objectType: Object type that is being commented on.
+     * @param {string} ObjectPk:  Object PK!
+     * @memberOf tiwun.sushial.services.CommentService
+     */
+    function filterByObject(objectType, ObjectPk) {
+        return $http.get(
+            ENV.apiEndpoint + 'sushial/comment/',
+            {
+                params: {
+                    object_type: objectType,
+                    object_pk: ObjectPk
+                }
+            }
+        );
+    }
+
+    return {
+        create: create,
+        remove: remove,
+        filterByObject: filterByObject,
+        ObjectTypes: ObjectTypes
+    };
+}
+
+angular.module('tiwun.sushial.services.CommentService', [])
+    .factory('CommentService', CommentService);
+
+CommentService.$inject = ['$http', 'ENV'];
