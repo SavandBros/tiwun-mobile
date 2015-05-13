3c143efd7ae67999bdbee18829cbba1181ff01b5
Alireza Savand <alireza.savand@gmail.com>
2015-05-13 22:44:20 +0400

    Check if the current logged in user has liked the last retrieved items

--- app/scripts/basement/controllers/index_controller.js
+++ app/scripts/basement/controllers/index_controller.js
@@ -5,11 +5,12 @@
  * @class IndexController
  * @namespace tiwun.basement.controllers.IndexController
  */
-function IndexController($scope, AuthenticationService, ItemService, ToastService) {
+function IndexController($scope, ToastService, AuthenticationService, ItemService, VoteService) {
     $scope.isAuthenticated = AuthenticationService.isAuthenticated();
     $scope.items = [];
     $scope.pageHasNext = true;
     $scope.pageCounter = 0;
+    $scope.auth = AuthenticationService.getAuthenticatedUser();
 
 
     /**
@@ -22,7 +23,6 @@ function IndexController($scope, AuthenticationService, ItemService, ToastServic
         ItemService.all(++$scope.pageCounter).then(
             function (data, status, headers, config) {
                 $scope.items = $scope.items.concat(data.data.classifies);
-
                 $scope.pageHasNext = data.data.page_has_next;
 
                 $scope.$broadcast('scroll.infiniteScrollComplete');
@@ -33,13 +33,38 @@ function IndexController($scope, AuthenticationService, ItemService, ToastServic
             }
         );
     };
+
+    /**
+     * Check if item has been liked by the current logged in user.
+     *
+     * If the user is not logged in, then check will be skipped.
+     */
+    $scope.$on('scroll.infiniteScrollComplete', function () {
+        if ($scope.auth) {
+            angular.forEach($scope.items.slice($scope.items.length - 5), function (item, v) {
+                VoteService.userVotedForObject(
+                    VoteService.objectTypes.item,
+                    item.pk,
+                    AuthenticationService.getAuthenticatedUser().pk
+                ).then(
+                    function (data, status, headers, config) {
+                        item.userVote = data.data;
+                    },
+                    function (data, status, headers, config) {
+                        console.log(data.data.error);
+                    }
+                );
+            });
+        }
+    });
 }
 
 angular.module('tiwun.basement.controllers.IndexController', [
     'tiwun.basement.services.ToastService',
     'tiwun.account.services.AuthenticationService',
-    'tiwun.item.services.ItemService'
+    'tiwun.item.services.ItemService',
+    'tiwun.sushial.services.VoteService'
 ])
     .controller('IndexController', IndexController);
 
-IndexController.$inject = ['$scope', 'AuthenticationService', 'ItemService', 'ToastService'];
+IndexController.$inject = ['$scope', 'ToastService', 'AuthenticationService', 'ItemService', 'VoteService'];
