cf1729d95c7ac88ce5363da8675ea7e1a23f6709
Alireza Savand <alireza.savand@gmail.com>
2015-05-13 22:36:33 +0400

    Refactor SingleItemController for humans

--- app/scripts/item/controllers/single_item_controller.js
+++ app/scripts/item/controllers/single_item_controller.js
@@ -1,71 +1,86 @@
+/*global angular*/
+'use strict';
+
 /**
  * SingleItemController
  *
+ * @class SingleItemController
  * @namespace tiwun.item.controllers
  **/
-(function () {
-    "use strict";
+function SingleItemController($scope, $stateParams, $ionicHistory, ItemService, CommentService, AuthenticationService) {
+    /**
+     * Actions to be performed when this controller is instantiated.
+     *
+     * @method constructor
+     * @memberOf tiwun.item.controllers.SingleItemController
+     **/
+    function constructor() {
+        ItemService.get($stateParams.itemId).then(
+            function (data, status, headers, config) {
+                $scope.context = data.data;
+                $scope.item = $scope.context.item;
 
-    angular.module('tiwun.item.controllers.SingleItemController', [
-        'tiwun.item.services.ItemService',
-        'tiwun.sushial.services.CommentService',
-        'tiwun.account.services.AuthenticationService'
-    ])
-        .controller('SingleItemController', SingleItemController);
+                CommentService.filterByObject(1, $scope.item.pk).then(
+                    function (data, status, headers, config) {
+                        $scope.item.comments = data.data;
+                    },
+                    function (data, status, headers, config) {
+                        console.log("[error] on getting comments!");
+                        console.log(data.error);
+                    }
+                );
+            },
+            function (data, status, headers, config) {
+                console.log('Error on receiving item');
+                console.log(data.error);
+                $ionicHistory.goBack();
+            }
+        );
+    }
 
-    SingleItemController.$inject = ['$scope', '$stateParams', '$ionicHistory', 'ItemService', 'CommentService', 'AuthenticationService'];
+    constructor();
 
     /**
-     * @class SingleItemController
-     * @namespace SingleItemController
+     * Add Comment to the item.
+     *
+     * @method addComment
+     * @param {Object} form
+     * @param {Object} comment
+     * @memberOf tiwun.item.controllers.SingleItemController
      */
-    function SingleItemController($scope, $stateParams, $ionicHistory, ItemService, CommentService, AuthenticationService) {
-        constructor();
+    $scope.addComment = function (form, comment) {
+        $scope.auth = AuthenticationService;
+        var user = $scope.auth.getAuthenticatedUser();
 
-        /**
-         * @name constructor
-         * @desc Actions to be performed when this controller is instantiated
-         * @memberOf tiwun.item.controllers.SingleItemController
-         **/
-        function constructor() {
-            ItemService.get($stateParams['itemId']).then(
+        if ($scope.auth.isAuthenticated()) {
+            console.log(form, comment);
+            CommentService.create(1, $scope.item.pk, user.pk, comment.text).then(
                 function (data, status, headers, config) {
-                    $scope.context = data.data;
-                    $scope.item = $scope.context['item'];
-
-                    CommentService.filterByObject(1, $scope.item.pk).then(
-                        function(data, status, headers, config){
-                            $scope.item.comments = data.data;
-                        },
-                        function(data, status, headers, config){
-                            console.log("[error] on getting comments!");
-                            console.log(data.error);
-                        }
-                    );
+                    $scope.item.comments = $scope.item.comments.concat(data.data);
                 },
                 function (data, status, headers, config) {
-                    console.log('Error on receiving item');
-                    console.log(data.error);
-                    $ionicHistory.goBack();
+                    console.log('commenting error');
+                    console.log(data.error)
                 }
             );
         }
-        $scope.addComment = function(form, comment) {
-            $scope.auth = AuthenticationService;
-            var user = $scope.auth.getAuthenticatedUser();
+    };
+}
 
-            if ($scope.auth.isAuthenticated()) {
-                console.log(form, comment);
-                CommentService.create(1, $scope.item.pk, user.pk, comment.text).then(
-                    function (data, status, headers, config) {
-                        $scope.item.comments = $scope.item.comments.concat(data.data);
-                    },
-                    function (data, status, headers, config) {
-                        console.log('commenting error');
-                        console.log(data.error)
-                    }
-                );
-            }
-        }
-    }
-})();
+
+angular.module('tiwun.item.controllers.SingleItemController', [
+    'tiwun.item.services.ItemService',
+    'tiwun.sushial.services.CommentService',
+    'tiwun.account.services.AuthenticationService'
+])
+    .controller('SingleItemController', SingleItemController);
+
+SingleItemController.$inject = [
+    '$scope',
+    '$stateParams',
+    '$ionicHistory',
+    'ItemService',
+    'CommentService',
+    'AuthenticationService'
+];
+
