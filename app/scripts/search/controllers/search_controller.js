9274e283c04949bc5a5adaae72807f5c2d7f6345
Alireza Savand <alireza.savand@gmail.com>
2015-05-17 19:27:03 +0400

    Search all over the place

--- app/scripts/search/controllers/search_controller.js
+++ app/scripts/search/controllers/search_controller.js
@@ -5,19 +5,60 @@
  * Search Controller
  *
  * @class SearchController
+ * @param {SearchService} SearchService
  * @param $scope
+ * @param $log
  * @namespace tiwun.search.controllers.SearchController
  */
-function SearchController($scope) {
+function SearchController($scope, $log, SearchService) {
+    var pageHasNext = true;
+    var pageNumber = 0;
+    var searchQuery;
     $scope.items = [];
 
     /**
-     * @name search
-     * @param query Search query
+     * Search all over the place!
+     *
+     * @method search
+     * @param {Object} form
+     * @param {Object} query Search query
+     * @memberOf tiwun.search.controllers.SearchController
      */
-    $scope.search = function (query) {
-        // TODO: What happened to search???? WHAAAAAAAAAAAT ?
-    }
+    $scope.search = function (form, query) {
+        pageNumber = 1;
+        searchQuery = query;
+
+        SearchService.search(query, pageNumber).then(
+            function (data, status, headers, config) {
+                $scope.items = data.data.classifies;
+                $scope.pageHasNext = data.data.page_has_next;
+            },
+            function (data, status, headers, config) {
+                $log.error("Error in search: " + data.data.error);
+            }
+        );
+    };
+
+    /**
+     * Load More
+     *
+     * @method loadMore
+     * @memberOf tiwun.search.controllers.SearchController
+     */
+    $scope.loadMore = function () {
+        SearchService.search(searchQuery, ++pageNumber).then(
+            function (data, status, headers, config) {
+                $scope.items = $scope.items.concat(data.data.classifies);
+                $scope.pageHasNext = data.data.page_has_next;
+
+                $scope.$broadcast('scroll.infiniteScrollComplete');
+            },
+            function (data, status, headers, config) {
+                $log.error("Error in search: " + data.data.error);
+            }
+        );
+    };
+
 }
 
 angular.module('tiwun.search.controllers.SearchController', [
@@ -25,4 +66,4 @@ angular.module('tiwun.search.controllers.SearchController', [
 ])
     .controller('SearchController', SearchController);
 
-SearchController.$inject = ['$scope'];
+SearchController.$inject = ['$scope', '$log', 'SearchService'];
