5a2cb434648fa123460fbac746808ae1d028dbe7
Alireza Savand <alireza.savand@gmail.com>
2015-05-20 16:11:54 +0400

    Code styling for item module registry

--- app/scripts/item/item.js
+++ app/scripts/item/item.js
@@ -1,16 +1,16 @@
-(function () {
-    'use strict';
+/*global angular*/
+'use strict';
 
-    angular.module('tiwun.item', [
-        'tiwun.item.controllers.ItemsControllers',
-        'tiwun.item.controllers.NewItemController',
-        'tiwun.item.controllers.SingleItemController',
-        'tiwun.item.services.ItemService'
-    ]);
 
-    angular.module('tiwun.item.controllers.ItemsControllers', []);
-    angular.module('tiwun.item.controllers.NewItemController', []);
-    angular.module('tiwun.item.controllers.SingleItemController', []);
-    angular.module('tiwun.item.services.ItemService', []);
-})();
 
+angular.module('tiwun.item', [
+    'tiwun.item.controllers.ItemsControllers',
+    'tiwun.item.controllers.NewItemController',
+    'tiwun.item.controllers.SingleItemController',
+    'tiwun.item.services.ItemService'
+]);
+
+angular.module('tiwun.item.controllers.ItemsControllers', []);
+angular.module('tiwun.item.controllers.NewItemController', []);
+angular.module('tiwun.item.controllers.SingleItemController', []);
+angular.module('tiwun.item.services.ItemService', []);
