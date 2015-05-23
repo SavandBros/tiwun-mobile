1413f8f423eb76bbf2096c6f78f43901142db3e9
Alireza Savand <alireza.savand@gmail.com>
2015-05-23 17:34:41 +0400

    Code clean up and some tiny refactoring

--- app/scripts/item/services/item_service.js
+++ app/scripts/item/services/item_service.js
@@ -1,95 +1,104 @@
+/*global angular*/
+'use strict';
+
 /**
- * ItemService
- * @namespace tiwun.item.services
+ * Item Service
+ *
+ * @param $http
+ * @param gettext
+ * @param ENV
+ * @class ItemService
+ * @namespace tiwun.item.services.ItemService
  */
-(function() {
-    'use strict';
-
-    angular.module('tiwun.item.services.ItemService', [])
-        .factory('ItemService', ItemService);
-
-    ItemService.$inject = ['$http', 'ENV'];
+function ItemService($http, gettext, ENV) {
+    var itemQueryFilters = {
+        1: gettext('Newest'),
+        2: gettext('Comments'),
+        3: gettext('Up Votes'),
+        4: gettext('Down Votes'),
+        5: gettext('Rate')
+    };
 
     /**
-     * @namespace ItemService
-     * @returns {Factory}
+     * @name all
+     * @desc Get all Items
+     * @param {Number} pageNumber
+     * @param {Number} queryFilter
+     * @returns {Promise}
+     * @memberOf tiwun.item.services.ItemService
      */
-    function ItemService($http, ENV) {
-        var ItemQueryFilters = {
-            1: 'Newest',
-            2: 'Comments',
-            3: 'Up Votes',
-            4: 'Down Votes',
-            5: 'Rate'
-        };
-
-        /**
-         * @name all
-         * @desc Get all Items
-         * @param {Number} pageNumber
-         * @param {Number} queryFilter
-         * @returns {Promise}
-         * @memberOf tiwun.item.services.ItemService
-         */
-        function all(pageNumber, queryFilter) {
-            return $http.get(
-                ENV.apiEndpoint + 'index/', {
-                    params: {
-                        page: pageNumber,
-                        filter: queryFilter
-                    }
+    function all(pageNumber, queryFilter) {
+        return $http.get(
+            ENV.apiEndpoint + 'index/', {
+                params: {
+                    page: pageNumber,
+                    filter: queryFilter
                 }
-            );
-        }
-
-
-        /**
-         * @name create
-         * @desc Create a new Post
-         * @param {{}} item The content of the new Post
-         * @returns {Promise}
-         * @memberOf tiwun.item.services.ItemService
-         */
-        function create(item) {
-            return $http.post(ENV.apiEndpoint + 'items/', {
-                title: item.title,
-                description: item.description,
-                tags: item.tags
-            });
-        }
-
-        /**
-         * @name Update
-         * @desc Update a single item
-         * @param {string} item The content of the item to be updated.
-         * @returns {Promise}
-         * @memberOf tiwun.item.services.ItemService
-         */
-        function update(item) {
-            return $http.put(ENV.apiEndpoint + 'items/', {
-                title: item.title,
-                description: item.description,
-                tags: item.tags
-            });
-        }
-
-        /**
-         * @name get
-         * @desc Get the ItemService of a given user
-         * @param {string} username The username to get ItemService for
-         * @returns {Promise}
-         * @memberOf tiwun.item.services.ItemService
-         */
-        function get(id) {
-            return $http.get(ENV.apiEndpoint + 'items/' + id + '/');
-        }
-
-        return  {
-            all: all,
-            get: get,
-            create: create,
-            update: update,
-            ItemQueryFilters: ItemQueryFilters
-        }
+            }
+        );
     }
-})();
+
+
+    /**
+     * create
+     *
+     * Create a new Post
+     *
+     * @method create
+     * @param {{}} item The content of the new Post
+     * @returns {Promise}
+     * @memberOf tiwun.item.services.ItemService
+     */
+    function create(item) {
+        return $http.post(ENV.apiEndpoint + 'items/', {
+            title: item.title,
+            description: item.description,
+            tags: item.tags
+        });
+    }
+
+    /**
+     * update
+     *
+     * Update a single item
+     *
+     * @method Update
+     * @param {string} item The content of the item to be updated.
+     * @returns {Promise}
+     * @memberOf tiwun.item.services.ItemService
+     */
+    function update(item) {
+        return $http.put(ENV.apiEndpoint + 'items/', {
+            title: item.title,
+            description: item.description,
+            tags: item.tags
+        });
+    }
+
+    /**
+     * get
+     *
+     * Get item by given ID.
+     *
+     * @method get
+     * @param {Number} id of the Item should be get from API.
+     * @returns {Promise}
+     * @memberOf tiwun.item.services.ItemService
+     */
+    function get(id) {
+        return $http.get(ENV.apiEndpoint + 'items/' + id + '/');
+    }
+
+    return  {
+        all: all,
+        get: get,
+        create: create,
+        update: update,
+        itemQueryFilters: itemQueryFilters
+    }
+}
+
+angular.module('tiwun.item.services.ItemService', [])
+    .factory('ItemService', ItemService);
+
+ItemService.$inject = ['$http', 'ENV'];
