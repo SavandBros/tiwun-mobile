3ec4b12c94f479d1e7d5fba62727b65a3a48e317
Alireza Savand <alireza.savand@gmail.com>
2015-05-29 21:09:46 +0400

    Code styling

--- app/scripts/item/controllers/new_item_controller.js
+++ app/scripts/item/controllers/new_item_controller.js
@@ -1,70 +1,66 @@
+/*global angular*/
+'use strict';
+
 /**
  * NewItemController
- * @namespace tiwun.item.controllers
+ *
+ * @class NewItemController
+ * @namespace tiwun.item.controllers.NewItemController
  */
-(function() {
-    'use strict';
+function NewItemController($rootScope, $scope, $state, $ionicHistory, $log, AuthenticationService, ItemService) {
+    $scope.auth = AuthenticationService;
 
-    angular.module('tiwun.item.controllers.NewItemController', [
-            'tiwun.item.services.ItemService',
-            'tiwun.account.services.AuthenticationService'
-        ])
-        .controller('NewItemController', NewItemController);
-
-    NewItemController.$inject = ['$rootScope', '$scope', '$state', '$ionicHistory', '$log', 'AuthenticationService', 'ItemService'];
-
-    /**
-     * @namespace NewItemController
-     */
-    function NewItemController($rootScope, $scope, $state, $ionicHistory, $log, AuthenticationService, ItemService) {
-        constructor();
-
-        $scope.auth = AuthenticationService;
-        $scope.IsPostPage = true;
-
-        function constructor() {
-            if (!AuthenticationService.isAuthenticated()) {
-                $state.go('app.login');
-            }
-        }
-
-        /**
-         * @name create
-         * @desc Create a new Item
-         * @memberOf tiwun.item.controllers.NewItemController
-         */
-        $scope.create = function(form) {
-            $scope.formSubmitted = true;
-
-            if (form.$valid) {
-                var item = {
-                    title: form.title.$modelValue,
-                    description: form.description.$modelValue,
-                    tags: form.tags.$modelValue
-                };
-
-                ItemService.create(item).then(
-                    function(data, status, headers, config) {
-                        $state.go('app.singleItem', {
-                            itemId: data.data.id
-                        });
-                    },
-                    function(data, status, headers, config) {
-                        $log.error('Error happened on creating new item: ' + data.error);
-                    }
-                )
-            }
-        };
-
-
-        /**
-         * Cancel
-         * @name cancel
-         * @desc Cancel creation of new item and go back!
-         * @memberOf tiwun.item.controllers.NewItemController
-         */
-        $scope.cancel = function() {
-            $ionicHistory.goBack();
+    function constructor() {
+        if (!AuthenticationService.isAuthenticated()) {
+            $state.go('app.login');
         }
     }
-})();
+
+    constructor();
+    /**
+     * @name create
+     * @desc Create a new Item
+     * @memberOf tiwun.item.controllers.NewItemController
+     */
+    $scope.create = function(form) {
+        $scope.formSubmitted = true;
+
+        if (form.$valid) {
+            var item = {
+                title: form.title.$modelValue,
+                description: form.description.$modelValue,
+                tags: form.tags.$modelValue
+            };
+
+            ItemService.create(item).then(
+                function(data, status, headers, config) {
+                    $state.go('app.singleItem', {
+                        itemId: data.data.id
+                    });
+                },
+                function(data, status, headers, config) {
+                    $log.error('Error happened on creating new item: ' + data.error);
+                }
+            )
+        }
+    };
+
+    /**
+     * Cancel
+     * @name cancel
+     * @desc Cancel creation of new item and go back!
+     * @memberOf tiwun.item.controllers.NewItemController
+     */
+    $scope.cancel = function() {
+        $ionicHistory.goBack();
+    }
+}
+
+
+angular.module('tiwun.item.controllers.NewItemController', [
+        'tiwun.item.services.ItemService',
+        'tiwun.account.services.AuthenticationService'
+    ])
+    .controller('NewItemController', NewItemController);
+
+NewItemController.$inject = ['$rootScope', '$scope', '$state', '$ionicHistory', '$log', 'AuthenticationService', 'ItemService'];
