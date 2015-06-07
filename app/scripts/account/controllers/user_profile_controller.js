9facb5c486fadfada7a22ca33181a07a87b796c3
Alireza Savand <alireza.savand@gmail.com>
2015-06-07 15:37:11 +0400

    Get items for the user profile
    @amirm3hdi take it over from here ;)

--- app/scripts/account/controllers/user_profile_controller.js
+++ app/scripts/account/controllers/user_profile_controller.js
@@ -7,8 +7,8 @@
  * @class UserProfileController
  * @namespace tiwun.account.controllers.UserProfileController
  */
-function UserProfileController($scope, $stateParams, $log, UserService) {
-    $scope.activeTab = "about";
+function UserProfileController($scope, $stateParams, $log, UserService, ItemService) {
+    $scope.activeTab = "items";
 
     /**
      * Actions to be performed when this controller is instantiated
@@ -30,16 +30,30 @@ function UserProfileController($scope, $stateParams, $log, UserService) {
     $scope.activateTab = function(tab) {
         if (tab != $scope.activeTab) {
             $scope.activeTab = tab;
+
+            if (tab === 'items') {
+                ItemService.items(1, {
+                    user_id: $scope.profile.id
+                }).then(
+                    function(data, status, headers, config) {
+                        $scope.profileItems = data.data.items
+                    },
+                    function(data, status, headers, config) {
+                        $log.error('Error in getting user items: ' + data.error);
+                    }
+                );
+            }
         }
-    }
+    };
 
     constructor();
 }
 
 
 angular.module('tiwun.account.controllers.UserProfileController', [
-        'tiwun.account.services.UserService'
+        'tiwun.account.services.UserService',
+        'tiwun.item.services.ItemService'
     ])
     .controller('UserProfileController', UserProfileController);
 
-UserProfileController.$inject = ['$scope', '$stateParams', '$log', 'UserService'];
+UserProfileController.$inject = ['$scope', '$stateParams', '$log', 'UserService', 'ItemService'];
